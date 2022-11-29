d781d5486a483db21c2fbb69161236625ce85aec
Ren <rpaczkowski@gmail.com>
2022-11-29 00:29:20 -0500

    Fixed App.tsx type error

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import AppContainer from "./components/AppContainer";
 import AppHeader from "./components/AppHeader";
 
 function App() {
-  <AppContainer header={<AppHeader />}>
-    ; return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-  </AppContainer>;
+  return (
+    <AppContainer header={<AppHeader />}>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    </AppContainer>
+  );
 }
 
 export default App;
